764b8e8e7a75f4b06ef00ae07c5a0f3e11d6ae06
Himanshu verma <98012299+TechisHeaven@users.noreply.github.com>
2024-07-30 10:25:30 +0530

    Testing Fixing Path Error in SidebarMain

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,7 @@
 import { usePathname } from "next/navigation";
 import Header from "@/components/ui/Header/Header";
 import React from "react";
-import SidebarMain from "../../components/ui/Sidebar/SidebarMain";
+import SidebarMain from "@/components/ui/Sidebar/SidebarMain";
 interface LayoutProps {
   children: React.ReactNode;
   table: React.ReactNode;
@@ -10,7 +10,6 @@ interface LayoutProps {
 export default function Layout({ children, table }: LayoutProps) {
   const pathname = usePathname();
   const isHomePage = pathname === "/";
-  const isSettingsRoute = pathname.startsWith("/settings");
   return (
     <div className="inline-flex w-full relative ">
       <SidebarMain />
